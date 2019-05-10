927ecb37482da2d5508b79eaeb168b37392111a9
Olivia Osborn <olioz95@gmail.com>
2019-05-10 11:02:45 -0400

    first commit

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,10 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
 
-  def get(self):
-    pass
+    def get(self):
+        pass
